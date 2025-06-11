a19e296d6ed702554bc1c7ca55c284aefdd6bae4
suyognatake <suyognatake7101999@gmail.com>
2025-06-11 23:53:28 +0530

    new chnage

--- tests/student.test.js
+++ tests/student.test.js
@@ -3,12 +3,12 @@ const app = require('../app');
 
 it('POST /student/addStudent → should create a student', async () => {
   const res = await request(app).post('/student/addStudent').send({
-    name: 'Test Student11',
+    name: 'Test Student12',
     email: 'admin@mail.com',
     phone: 9284220326,
     gender: "male"
   });
 
   expect(res.statusCode).toBe(201);
-  expect(res.body.createdStudent.name).toBe('Test Student11');
+  expect(res.body.createdStudent.name).toBe('Test Student12');
 });
