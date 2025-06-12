c9d38282a482d8a72e8797a8aec23d5bbf7b6f22
suyognatake <suyognatake7101999@gmail.com>
2025-06-12 10:10:14 +0530

    chnages

--- api/model/teacher.js
+++ api/model/teacher.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const teacherSchema = new mongoose.Schema({
-  _id: mangoose.Schema.Types.ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   name: String,
   email: String,
   phone: Number,
   gender: String,
+  department: String,
   age: Number,
   salary: Number,
   status: {
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const studentRoute = require('./api/routes/student');
 const facultyRoute = require('./api/routes/faculty');
+const teacherRoute = require('./api/routes/teacher');
 
 mongoose.connect('mongodb+srv://devsuyog1999:nM7gux8cJX7drsyC@student.pr2ig3m.mongodb.net/studentDB', {
   useNewUrlParser: true,
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/student', studentRoute);
 app.use('/faculty', facultyRoute);
+app.use('/teacher', teacherRoute);
 
 app.use((req, res, next) => {
   res.status(404).json({
